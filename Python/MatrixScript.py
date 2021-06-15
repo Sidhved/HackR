028f7745b0577c14934d85a2608c9448b0aca7a3
Sidhved <sidhved.warik@gmail.com>
2021-06-15 20:45:34 +0530

    Added 20

---
+++ Python/MatrixScript.py
@@ -0,0 +1,37 @@
+'''
+To decode the script, Neo needs to read each column and select only the alphanumeric characters and connect them. Neo reads the column from top to bottom and starts reading from the leftmost column.
+
+If there are symbols or spaces between two alphanumeric characters of the decoded script, then Neo replaces them with a single space '' for better readability.
+
+Neo feels that there is no need to use 'if' conditions for decoding.
+
+Alphanumeric characters consist of: [A-Z, a-z, and 0-9].
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+first_multiple_input = input().rstrip().split()
+
+n = int(first_multiple_input[0])
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+
+# start   
+matrix = list(zip(*matrix))
+
+sample = str()
+
+for words in matrix:
+    for char in words:
+        sample += char
+       
+print(re.sub(r'(?<=\w)([^\w\d]+)(?=\w)', ' ', sample)) 
