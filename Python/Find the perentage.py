300f1d2f312e5e31dcdb28be0f201b4004165213
Sidhved <sidhved.warik@gmail.com>
2021-06-15 20:20:13 +0530

    Added 10

---
+++ Python/Find the perentage.py
@@ -0,0 +1,21 @@
+'''
+The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+'''
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    a = student_marks[query_name]
+    sum = 0
+    for i in a:
+        sum += i
+
+    sum /= len(a)
+    sum = "{:.2f}".format(sum)
+
+    print(sum)
