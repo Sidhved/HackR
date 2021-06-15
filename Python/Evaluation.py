db89525582c2bd61d7c2ba85d6e0cf3a60e7caee
Sidhved <sidhved.warik@gmail.com>
2021-06-15 20:45:15 +0530

    Added 18

---
+++ Python/Evaluation.py
@@ -0,0 +1,8 @@
+'''
+Task
+You are given an expression in a line. Read that line as a string variable, such as var, and print the result using eval(var).
+'''
+
+if __name__ == "__main__":
+    var = input()
+    eval(var)
