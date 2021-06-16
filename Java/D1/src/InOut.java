fc3d5ec3aa831a7bfc33ff3e4ea964c5c2be81b2
Sidhved <sidhved.warik@gmail.com>
2021-06-16 19:42:22 +0530

    Added 3

---
+++ Java/D1/src/InOut.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.util.Scanner;
+
+public class InOut {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i);
+        i = sc.nextInt();
+        System.out.println(i);
+        i = sc.nextInt();
+        System.out.println(i);
+        sc.close();
+    }
+}
