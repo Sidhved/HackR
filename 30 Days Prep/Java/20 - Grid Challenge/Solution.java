f81735f0b42066ee35cc421b13d7686ba05608c9
Sidhved <sidhved.warik@gmail.com>
2022-01-08 19:12:04 +0530

    Day 8

---
+++ 30 Days Prep/Java/20 - Grid Challenge/Solution.java
@@ -0,0 +1,7 @@
+class Solution{
+    public static void main(String[] args) {
+        System.out.println("Need Help With This Question");
+        System.out.println("Find the original Question on Hackerrank - Grid Challenge");
+        System.out.println("Link: https://www.hackerrank.com/challenges/one-month-preparation-kit-grid-challenge/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-two");
+    }
+}
