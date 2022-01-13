ac5a8f3e045d0844680a4bf1558a8909b9cc3e30
Sidhved <sidhved.warik@gmail.com>
2022-01-13 15:06:32 +0530

    Day 13

---
+++ 30 Days Prep/Java/32 - Insert a node in linked list/Solution.java
@@ -0,0 +1,2 @@
+// Need help with this question.
+// The question can be found at : https://www.hackerrank.com/challenges/one-month-preparation-kit-insert-a-node-at-a-specific-position-in-a-linked-list/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-three
